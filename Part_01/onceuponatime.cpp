f1de8841eef7bfbd4fc3179ec24a1d7cd3ddc553
Syed Azeez <syedazeez337@gmail.com>
2024-03-29 20:58:23 +0530

    second programme

---
+++ Part_01/onceuponatime.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+
+int main(void)
+{
+    // Use three cout commands for printing
+    std::cout << "Once upon a time\n";
+    std::cout << "there was\n";
+    std::cout << "a programme\n";
+}
